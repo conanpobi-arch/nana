f4e53d464e95abc21572249e16c81396aac23af6
conanpobi-arch <gyerimpa@gmail.com>
2026-02-23 21:19:33 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,25 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
+// 알람 메시지 수신
+self.addEventListener('message', function(e) {
+  if (e.data && e.data.type === 'SCHEDULE_ALARM') {
+    var alarm = e.data.alarm;
+    var diff = new Date(alarm.date + 'T' + alarm.time).getTime() - Date.now();
+    if (diff > 0) {
+      setTimeout(function() {
+        self.registration.showNotification('🔔 NANA LAB 알람', {
+          body: alarm.title + (alarm.memo ? '\n' + alarm.memo : ''),
+          vibrate: [300, 100, 300, 100, 300],
+          tag: 'alarm-' + alarm.id,
+          requireInteraction: true
+        });
+      }, diff);
+    }
+  }
+});
+
+self.addEventListener('notificationclick', function(e) {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/todo.html'));
+});
